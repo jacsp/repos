e1c2a66b067992185d2fcd4c7c922d186e2cce13
jacsp <jspr54132@edu.ucl.dk>
2023-02-09 09:27:48 +0100

    Initial commit

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Bruger input
+        Console.Write("Indtast navn: ");
+        string? navn = Console.ReadLine();
+        Console.Write("Indtast alder: ");
+        string? alder = Console.ReadLine();
+
+        // Print to console
+        Console.WriteLine(navn + " er " + alder + " år gammel.");
+    }
+}
