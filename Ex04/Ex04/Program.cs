e8a89446b6b5b10dd4abf53ff2f07b5d44746d73
jacsp <jspr54132@edu.ucl.dk>
2023-02-19 16:41:04 +0100

    Det meste af Ex04

---
+++ Ex04/Ex04/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Øvelse 2.5
+            //string _coolString = "Many letter in a string";
+            //int i = 0;
+
+            //while (i < _coolString.Length)
+            //{
+            //    Console.WriteLine(_coolString[i]);
+            //    i++;
+            //}
+
+            // Øvelse 2.6
+            //string _coolString = "Another cool string";
+            //int i = 0;
+
+            //do
+            //{
+            //    Console.WriteLine(_coolString[i]);
+            //    i++;
+            //}
+            //while (i < _coolString.Length);
+
+            // Øvelse 2.7
+            //int _num1, _num2;
+            //int _a = 10;
+            //int _b = 7;
+
+            //_num1 = _a / _b;
+            //_num2 = _a % _b;
+
+            //Console.WriteLine("Heltalskvotient: {0}\nRest-delen: {1}", _num1, _num2);
+
+            // Øvelse 2.8
+            // Er for doven, men ville omringe koden i en while loop, som spørger, om man vil forstætte eller ej hver gang programmet havde exited
+            // Brug Console.Clear(); til at clear console
+
+            // Øvelse 2.9
+            //string _coolString = "Many letter in a string";
+            //int i = 0;
+
+            //while (i < _coolString.Length)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(_coolString[i]);
+            //    }
+            //    i++;
+            //}
+
+            // Øvelse 2.10
+            //string _lettersAndNums = "Den 3her stri7ng indeholder4 tal8";
+
+            //for (int i = 0; i <_lettersAndNums.Length; i++)
+            //{
+            //    if (Char.IsNumber(_lettersAndNums[i]))
+            //    {
+            //        Console.WriteLine("{0}: {1}", i, _lettersAndNums[i]);
+            //    }
+            //}
+
+            // Øvelse 2.11
+            //string _lettersAndNums = "Den 3h-er st-ri7ng+ indehold+er4 tal8";
+
+            //for (int i = 0; i < _lettersAndNums.Length; i++)
+            //{
+            //    if (Char.IsNumber(_lettersAndNums[i]))
+            //    {
+            //        Console.WriteLine("{0}: {1} (number)", i, _lettersAndNums[i]);
+            //    }
+            //    else if (_lettersAndNums[i] == '+' || _lettersAndNums[i] == '-')
+            //    {
+            //        Console.WriteLine("{0}: {1} (operator)", i, _lettersAndNums[i]);
+            //    }
+            //}
+
+            // Øvelse 2.12
+            string _inputString = Console.ReadLine();
+        }
+    }
+}
