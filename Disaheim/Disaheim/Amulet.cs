645e10faaac951ee11c40e763c19a1361aee86ee
jacsp <jspr54132@edu.ucl.dk>
2023-03-22 16:59:39 +0100

    Ex15

---
+++ Disaheim/Disaheim/Amulet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Amulet
+    {
+        string itemId;
+        string design;
+        Level quality;
+
+        public string ItemId
+        {
+            get { return itemId; }
+            set { itemId = value; }
+        }
+
+        public string Design
+        {
+            get { return design; }
+            set { design = value; }
+        }
+
+        public Level Quality
+        {
+            get { return quality; }
+            set { quality = value; }
+        }
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            this.ItemId = itemId;
+            this.Quality = quality;
+            this.Design = design;
+        }
+
+        public Amulet(string itemId, Level quality): 
+            this(itemId, quality, "") 
+        {
+        }
+
+        public Amulet(string itemId): 
+            this(itemId, Level.medium, "") 
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"ItemId: {itemId}, Quality: {quality}, Design: {design}";
+        }
+    }
+}
---
+++ Disaheim/Disaheim/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Disaheim
+{
+    public class Book
+    {
+        string itemId;
+        string title;
+        double price;
+
+        public string ItemId
+        {
+            get { return itemId; }
+            set { itemId = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public Book(string itemId, string title, double price)
+        {
+            this.ItemId = itemId;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public Book(string itemId, string title) : this(itemId, title, 0) { }
+
+        public Book(string itemId) : this(itemId, "", 0) { }
+
+        public override string ToString()
+        {
+            return $"ItemId: {itemId}, Title: {title}, Price: {price}";
+        }
+    }
+}
