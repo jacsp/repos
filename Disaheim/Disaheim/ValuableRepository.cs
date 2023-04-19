6994b6fc1e09a5f60e22b0fce6a21a5de4d94db5
jacsp <jspr54132@edu.ucl.dk>
2023-04-19 22:20:22 +0200

    Ex19
    Finally done. Alt virker som det skal.

--- Disaheim/Disaheim/ValuableRepository.cs
+++ Disaheim/Disaheim/ValuableRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Disaheim
 {
-    public class ValuableRepository
+    public class ValuableRepository : IPersistable
     {
         private List<IValuable> valuables = new List<IValuable>();
 
@@ -86,5 +86,67 @@ namespace Disaheim
             return valuables.Count;
         }
 
+        public void Save(string fileName = "ValuableRepository")
+        {
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                foreach (var valuable in valuables)
+                {
+                    if (typeof(Course) == valuable.GetType())
+                    {
+                        Course course = (Course)valuable;
+                        sw.WriteLine($"COURSE;{course.Name};{course.DurationInMinutes}");
+                    }
+                    else if (typeof(Book) == valuable.GetType())
+                    {
+                        Book book = (Book)valuable;
+                        sw.WriteLine($"BOOK;{book.ItemId};{book.Title};{book.Price}");
+                    }
+                    else if (typeof(Amulet) == valuable.GetType())
+                    {
+                        Amulet amulet = (Amulet)valuable;
+                        sw.WriteLine($"AMULET;{amulet.ItemId};{amulet.Quality};{amulet.Design}");
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+        }
+
+        public void Load(string fileName = "ValuableRepository")
+        {
+            string line;
+            string[] values;
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    values = line.Split(";");
+
+                    if (values[0] == "COURSE")
+                    {
+                        Course course = new(values[1], int.Parse(values[2]));
+                        valuables.Add(course);
+                    }
+                    else if (values[0] == "BOOK")
+                    {
+                        Book book = new(values[1], values[2], double.Parse(values[3]));
+                        valuables.Add(book);
+                    }
+                    else if (values[0] == "AMULET")
+                    {
+                        Amulet amulet = new(values[1], (Level)Enum.Parse(typeof(Level), values[2]), values[3]);
+                        valuables.Add(amulet);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
     }
 }
