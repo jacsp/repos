0adc00e933be242c1bd59947eff295af7f209388
jacsp <jspr54132@edu.ucl.dk>
2023-04-24 12:33:50 +0200

    (Ikke færdig) Ex20 CustomExceptionHandling

---
+++ CustomExceptionHandling/CustomExceptionHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using TotallySafeLib;
+
+namespace CustomExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int posValue = TotallySafe.GetValueAtPosition(-2);
+
+                Console.WriteLine(posValue);
+                Console.ReadLine();
+            }
+            catch (NegativeIndexOutOfRangeException ex)
+            {
+                
+            }
+        }
+    }
+}
---
+++ CustomExceptionHandling/TotallySafeLib/TotallySafe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace TotallySafeLib
+{
+    public class TotallySafe
+    {
+        public static double Divider(int number)
+        {
+            return 7 / number;
+        }
+        public static int StringToInt(string stringToConvert)
+        {
+            return int.Parse(stringToConvert);
+        }
+        public static int GetValueAtPosition(int positionInArray)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            return intArray[positionInArray];
+        }
+    }
+}
