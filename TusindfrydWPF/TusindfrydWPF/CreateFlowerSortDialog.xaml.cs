403db70215a378c4bad528f3a2e09247a82de96d
jacsp <jspr54132@edu.ucl.dk>
2023-05-06 02:22:25 +0200

    Ex25 WPFDialogGUI done

---
+++ TusindfrydWPF/TusindfrydWPF/CreateFlowerSortDialog.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TusindfrydWPF
+{
+    /// <summary>
+    /// Interaction logic for CreateFlowerSortDialog.xaml
+    /// </summary>
+    public partial class CreateFlowerSortDialog : Window
+    {
+        public string Name { get; set; }
+        public string PicturePath { get; set; }
+        public int ProductionTime { get; set; }
+        public int HalfLifeTime { get; set; }
+        public int Size { get; set; }
+
+        bool nameEntered = false;
+        bool picturePathEntered = false;
+        bool productionTimeEntered = false;
+        bool halfTimeEntered = false;
+        bool sizeEntered = false;
+
+        public CreateFlowerSortDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            Name = tbName.Text;
+            PicturePath = tbPicturePath.Text;
+            try
+            {
+                ProductionTime = int.Parse(tbProductionTime.Text);
+                HalfLifeTime = int.Parse(tbHalfTime.Text);
+                Size = int.Parse(tbSize.Text);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            DialogResult = true;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void tbName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enableOkBtnCheck();
+            if (tbName.Text.Length > 0)
+            {
+                nameEntered = true;
+            }
+        }
+
+        private void tbPicturePath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enableOkBtnCheck();
+            if (tbPicturePath.Text.Length > 0)
+            {
+                picturePathEntered = true;
+            }
+
+            try
+            {
+                // Create a new bitmap image to create a path
+                BitmapImage bitMapImage = new();
+                bitMapImage.BeginInit();
+                bitMapImage.UriSource = new(tbPicturePath.Text, UriKind.Absolute);
+                bitMapImage.EndInit();
+
+                imgFlower.Source = bitMapImage;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void tbProductionTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enableOkBtnCheck();
+            if (tbProductionTime.Text.Length > 0)
+            {
+                productionTimeEntered = true;
+            }
+        }
+
+        private void tbHalfTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enableOkBtnCheck();
+            if (tbHalfTime.Text.Length > 0)
+            {
+                halfTimeEntered = true;
+            }
+        }
+
+        private void tbSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enableOkBtnCheck();
+            if (tbSize.Text.Length > 0)
+            {
+                sizeEntered = true;
+            }
+        }
+
+        private void enableOkBtnCheck()
+        {
+            // Test if all textboxes have text in them
+            if (nameEntered && picturePathEntered && productionTimeEntered && halfTimeEntered && sizeEntered) btnOk.IsEnabled = true;
+            else btnOk.IsEnabled = false;
+        }
+    }
+}
---
+++ TusindfrydWPF/TusindfrydWPF/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TusindfrydWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<FlowerSort> flowerSorts;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            flowerSorts = new List<FlowerSort>();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CreateFlowerSortDialog createFlowerSort = new();
+            // Question mark is needed
+            bool? result = createFlowerSort.ShowDialog();
+
+            if (result == true)
+            {
+                // Take all information from the createflower class and make a new instance of flower
+                FlowerSort flower = new();
+                flower.Name = createFlowerSort.Name;
+                flower.PicturePath = createFlowerSort.PicturePath;
+                flower.ProductionTime = createFlowerSort.ProductionTime;
+                flower.HalfLifeTime = createFlowerSort.HalfLifeTime;
+                flower.Size = createFlowerSort.Size;
+                flowerSorts.Add(flower);
+            }
+            else
+            {
+                return;
+            }
+
+            // Update the textblock to show list of flowers
+            if (flowerSorts.Count > 0)
+            {
+                tbFlowerSorts.Text = "";
+                foreach (var flowerSort in flowerSorts)
+                {
+                    tbFlowerSorts.Text += $"{flowerSort.Name}\n";
+                }
+            }
+        }
+    }
+}
