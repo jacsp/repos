a641c396c26838ff0c352728dd38c969e32db665
jacsp <jspr54132@edu.ucl.dk>
2023-05-07 17:30:03 +0200

    Ex26 WPFAndMVVM done

---
+++ WPFAndMVVM1/WPFAndMVVM1/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPFAndMVVM1
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _myLabelText = "Text not set yet";
+
+        public string MyLabelText
+        {
+            get => _myLabelText;
+            set
+            {
+                _myLabelText = value;
+                OnPropertyChanged(nameof(MyLabelText));
+            }
+        }
+
+        private string _myTextBoxText = "Text not set yet";
+
+        public string MyTextBoxText
+        {
+            get => _myTextBoxText;
+            set
+            {
+                _myTextBoxText = value;
+                OnPropertyChanged(nameof(MyTextBoxText));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
