9fcd69b8ba7f8bce1c7ca9da06c026ac4341da9b
jacsp <jspr54132@edu.ucl.dk>
2023-10-10 09:49:47 +0200

    Create LambdaAndDelegates

---
+++ LambdaAndExtensionMethods/LambdaAndExtensionMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace LambdaAndExtensionMethods
+{
+    internal class Program
+    {
+        // Øvelse 2
+        //delegate double Calculate(string expression);
+        //delegate int Sequence(int num);
+        //delegate void Delete(int index);
+        //delegate string Get();
+        //delegate void Alert(string message);
+        //delegate bool Exists(int id);
+        //delegate void Area(int x, int y);
+
+        Func<string, double> Calculate;
+        Func<int, int> Sequence;
+        Action<int> Delete;
+        Func<string> Get;
+        Action<string> Alert;
+        Predicate<int> Exists;
+        Action<int, int> Area;
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
