a57af64d399cd14ace3975d882a068fe722c7b46
jacsp <jspr54132@edu.ucl.dk>
2023-11-01 09:03:06 +0100

    LINQ stuff

---
+++ LINQStuff/LINQStuff/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace LINQStuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Jacob", "Mikkel", "Nikolai", "Lars", "Oliver" };
+
+            //IEnumerable<string> filteredNames = names.Where(n => n.Length > 5);
+
+            IEnumerable<string> filteredNames = from n in names
+                                                where n.Contains('a')
+                                                select n;
+
+            IEnumerable<string> query =
+                from n in names
+                where n.Contains('a')
+                orderby n.Length
+                select n.ToUpper();
+
+
+
+            foreach (string name in query) Console.WriteLine(name);
+
+        }
+    }
+}
