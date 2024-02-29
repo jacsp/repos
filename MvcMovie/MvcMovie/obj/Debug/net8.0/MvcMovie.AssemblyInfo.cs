8c9918f60a6df8df9b777f58953d974710b9f921
jacsp <jspr54132@edu.ucl.dk>
2024-02-29 04:16:04 +0100

    asp.net mvc tutorial part 6

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57af64d399cd14ace3975d882a068fe722c7b46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6048d65c682b08a1e07ae74915d58e2e5425dad")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
